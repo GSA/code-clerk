da6962e105bc28cd62066953ed7d04abdaa5dc78
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2018-06-13 15:43:46 -0400

    add CLI

---
+++ bin/codeclerk.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+// const clerk = require("code-clerk")
+const clerk = require("../lib/index")
+// const argparse = require("argparse")
+const ArgumentParser = require("argparse").ArgumentParser
+const package = require("../package.json")
+
+let argParser = new ArgumentParser({
+  version: package.version,
+  addHelp: true,
+  description: package.description
+})
+
+argParser.addArgument([ "-t", "--token-var" ], {
+  help: "Environment variable containing GitHub token (default: GITHUB_ACCESS_TOKEN)",
+  defaultValue: "GITHUB_ACCESS_TOKEN",
+  nargs: 1
+})
+
+argParser.addArgument("AGENCY", {
+  help: "Your agency acronym",
+  nargs: 1
+})
+
+argParser.addArgument("GITHUB_ORG", {
+  help: "List of GitHub organization names to pull metadata from",
+  nargs: "+"
+})
+
+const args = argParser.parseArgs()
+
+if (!process.env[args.token_var]) {
+  console.log(`Please set $${args.token_var} to your GitHub access token (or use '-t')`)
+  process.exit(1)
+}
+
+const client = new clerk.GitHubClient(process.env[args.token_var])
+const inventory = new clerk.Inventory(client, args.AGENCY[0])
+
+inventory.build(args.GITHUB_ORG).then((data) => {
+  console.log(JSON.stringify(data))
+})
